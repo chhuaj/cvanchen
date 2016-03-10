028655aa78d79650fa62cef0d9405c75c87dcff2
chhuaj <815350282@qq.com>
2016-03-10 18:18:23 +0800

    first tornado

--- mian.py
+++ mian.py
@@ -2,10 +2,21 @@
 
 import os,sys
 
-def t():
-   print "hello"
-   yield 3
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
 
-t()
-print "word"
-print "hello"
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
